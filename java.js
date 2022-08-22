75e85501839beabdd4922cf61c3412270790ea54
vanshika0621 <100373512+vanshika0621@users.noreply.github.com>
2022-08-22 19:05:10 +0530

    my first apple project

---
+++ java.js
@@ -0,0 +1,13 @@
+// for( var num=1;num <= 10 ; num++){
+//     var tableof=5;
+//     console.log(tableof +"*" +num +"=" + tableof*num);
+
+
+//    a= 5;
+//     if(a < 10){
+//         console.log("the submission is successfull")
+//     }
+//     else{
+//         console.log("not posiible")
+//     
+    
